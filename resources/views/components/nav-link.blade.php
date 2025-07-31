2e17b93383a40f03c7064c09ce46b5411b6d8927
MKashanButt <kashanbutt566@gmail.com>
2025-07-31 17:26:00 -0400

    fix navigation not showing if not selected issue

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -5,7 +5,7 @@
     $classes =
         $active ?? false
             ? "flex items-center justify-between w-full p-2 text-xs font-medium text-[$text] bg-white border-white rounded-lg"
-            : 'flex items-center justify-between w-full p-2 py-1 text-xs font-medium bg-transparrent border-2 border-white text-white border-white rounded-lg';
+            : 'flex items-center justify-between w-full p-2 py-1 text-xs font-medium bg-transparrent border-2 border-white border-white rounded-lg';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
