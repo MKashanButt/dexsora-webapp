6571161efec45b6affd239d1838867c578306596
MKashanButt <kashanbutt566@gmail.com>
2025-07-31 17:29:30 -0400

    add ability to render sheet count

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -57,12 +57,19 @@
                     @foreach ($sheets as $key => $sheet)
                         <x-nav-link :href="route('index', $key)" :active="request()->routeIs('index')">
                             {{ $sheet }}
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail">
-                                <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-                                <rect x="2" y="4" width="20" height="16" rx="2" />
-                            </svg>
+                            @if ($sheetDataCount[$key] > 0)
+                                <p class="px-2 py-1 rounded-full bg-red-500 text-white mr-[1px]">
+                                    {{ $sheetDataCount[$key] }}
+                                </p>
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-mail-icon lucide-mail">
+                                    <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+                                    <rect x="2" y="4" width="20" height="16" rx="2" />
+                                </svg>
+                            @endif
                         </x-nav-link>
                     @endforeach
                 </div>
