4175b7405fb204d3287fb0872c92a9aa8437ba41
MKashanButt <kashanbutt566@gmail.com>
2025-07-31 17:48:16 -0400

    add edit form

--- resources/views/render.blade.php
+++ resources/views/render.blade.php
@@ -77,9 +77,71 @@
                                                 </div>
                                             </div>
                                         @endforeach
+                                        <div x-data="{ open: false }">
+                                            <span @click="open=!open">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="lucide lucide-pencil-icon lucide-pencil">
+                                                    <path
+                                                        d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                                                    <path d="m15 5 4 4" />
+                                                </svg>
+                                            </span>
+                                            <div class="absolute top-0 left-0 w-full h-full bg-black/70 backdrop-blur-xs z-100 flex items-center justify-center"
+                                                x-show="open" x-cloak>
+                                                <div class="bg-white rounded-md p-2">
+                                                    <form method="POST"
+                                                        action="{{ route('update', ['data' => $item->id, 'field' => $fields['name']]) }}"
+                                                        enctype="multipart/form-data">
+                                                        @csrf
+                                                        @method('PATCH')
+                                                        <input type="{{ $fields['type'] }}"
+                                                            name="{{ in_array($fields['name'], ['document', 'pod']) ? $fields['name'] . '[]' : $fields['name'] }}"
+                                                            multiple
+                                                            class="border border-gray-300 px-2 py-1 text-xs rounded"
+                                                            placeholder="Enter {{ $fields['name'] }}"
+                                                            value="{{ $fields['name'] }}">
+                                                        <x-primary-button type="submit">Save</x-primary-button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 @else
-                                    {{ $value }}
+                                    <div class="flex gap-2 items-center">
+                                        {{ $value }}
+                                        <div x-data="{ open: false }">
+                                            <span @click="open=!open">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="lucide lucide-pencil-icon lucide-pencil">
+                                                    <path
+                                                        d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                                                    <path d="m15 5 4 4" />
+                                                </svg>
+                                            </span>
+                                            <div class="absolute top-0 left-0 w-full h-full bg-black/70 backdrop-blur-xs z-100 flex items-center justify-center"
+                                                x-show="open" x-cloak>
+                                                <div class="bg-white rounded-md p-2">
+                                                    <form method="POST"
+                                                        action="{{ route('update', ['data' => $item->id, 'field' => $fields['name']]) }}"
+                                                        enctype="multipart/form-data">
+                                                        @csrf
+                                                        @method('PATCH')
+                                                        <input type="{{ $fields['type'] }}"
+                                                            name="{{ in_array($fields['name'], ['document', 'pod']) ? $fields['name'] . '[]' : $fields['name'] }}"
+                                                            multiple
+                                                            class="border border-gray-300 px-2 py-1 text-xs rounded"
+                                                            placeholder="Enter {{ $fields['name'] }}"
+                                                            value="{{ $value }}">
+                                                        <x-primary-button type="submit">Save</x-primary-button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 @endif
                             </td>
                         @endforeach
